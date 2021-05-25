cd6097c6f7f0fdf8bad23b4bae5d0fb9ab44317c
Sen Cai <cs1159821786@gmail.com>
2021-05-25 09:43:51 +0800

    pass ini3

---
+++ ini3.py
@@ -0,0 +1,8 @@
+letters = 'cGI1uPMGBvKxf4Z64W3UcH7t55CsnPrunellaSYo4qsHKZJPdn2OCZUBoNALiOmoGaFCTaVN11A0j35RfxIiySFiphDBo1egYvVg9NzaHk2L5Vc1cZUKsdypHcRAY0QmelesdXzefxz92IuTdRxkc6LLX65A1WvWdk.'
+a = 29
+b = 36
+c = 127
+d = 131
+#out = letters[a:b] + '' + letters[c:d]
+print(letters[a:b+1], letters[c:d+1])
+
